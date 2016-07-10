0c50301965178141f6365ca65521f8ecb0ca9a91
Pouya Kary <k@karyfoundation.org>
2016-07-10 23:51:52 +0430

    updated....

---
+++ comment.js
@@ -0,0 +1,25 @@
+
+//
+// Comment VSCODE - Having a basic support for KFCS inside Visual Studio Code
+// Copyright 2016 Kary Foundation, Inc. All rights reserved.a
+//    Author: Pouya Kary <k@karyfoundation.org>
+//
+
+var vscode = require('vscode');
+
+function activate ( context ) {
+	var disposable = vscode.commands.registerCommand('commentVSCode.makeLineSectionComment', function () {
+		// The code you place here will be executed every time your command is executed
+
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World!');
+	});
+	
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {
+}
+exports.deactivate = deactivate;
