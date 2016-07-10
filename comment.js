d213f160220a9126610f746dd2811a24224c2aae
Pouya Kary <k@karyfoundation.org>
2016-07-11 01:46:54 +0430

    environmental information is here...

--- comment.js
+++ comment.js
@@ -1,25 +1,88 @@
-
 //
 // Comment VSCODE - Having a basic support for KFCS inside Visual Studio Code
 // Copyright 2016 Kary Foundation, Inc. All rights reserved.a
 //    Author: Pouya Kary <k@karyfoundation.org>
 //
 
-var vscode = require('vscode');
+"use strict";
 
-function activate ( context ) {
-	var disposable = vscode.commands.registerCommand('commentVSCode.makeLineSectionComment', function () {
-		// The code you place here will be executed every time your command is executed
+//
+// ─── IMPORTS ────────────────────────────────────────────────────────────────────
+//
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World!');
-	});
-	
-	context.subscriptions.push(disposable);
-}
-exports.activate = activate;
+    var vscode = require( 'vscode' );
 
-// this method is called when your extension is deactivated
-function deactivate() {
-}
-exports.deactivate = deactivate;
+//
+// ─── DEFS ───────────────────────────────────────────────────────────────────────
+//
+
+    var oneLineCommentSign;
+    var currentLine;
+    var currentLineString;
+    var currentLanguageId;
+    var currentInsertSpacesStatus;
+    var currentTabSize;
+    var lineFormat;
+
+//
+// ─── GET ENVIRONMENTAL INFORMATION ──────────────────────────────────────────────
+//
+
+    function loadEnvironmentalInformation (  ) {
+        currentLine                 = vscode.window.activeTextEditor.selection.start.line;
+        currentLineString           = vscode.window.activeTextEditor.document.lineAt( currentLine );
+        currentLanguageId           = vscode.window.activeTextEditor.document.languageId;
+        currentInsertSpacesStatus   = vscode.window.activeTextEditor.options.insertSpaces;
+        currentTabSize              = vscode.window.activeTextEditor.options.tabSize;
+        oneLineCommentSign          = getOneLineLanguageCommentSignForCurrentLanguage( );
+        lineFormat                  = generateRegExForCurrentLanguage( );
+    }
+
+//
+// ─── GENERATE REGEX FOR LANGUAGE ────────────────────────────────────────────────
+//
+
+    function generateRegExForCurrentLanguage (  ) {
+        return new RegExp( `\s*(${ oneLineCommentSign })?([a-zA-Z ]|[0-9][0-9\.]*)+\s*` );
+    }
+
+//
+// ─── GET ONE LINE COMMENT SIGN FOR CURRENT LANGUAGE ─────────────────────────────
+//
+
+    function getOneLineLanguageCommentSignForCurrentLanguage ( ) {
+        switch ( currentLanguageId ) {
+            default :
+                return '//';
+        }
+    }
+
+//
+// ─── BODY ───────────────────────────────────────────────────────────────────────
+//
+
+    function activate( context ) {
+        var disposable = vscode.commands.registerCommand(
+            'commentVSCode.makeLineSectionComment', ( ) => {
+                loadEnvironmentalInformation( );
+
+            });
+
+        context.subscriptions.push(disposable);
+    }
+
+    exports.activate = activate;
+
+//
+// ─── DEACTIVATE ─────────────────────────────────────────────────────────────────
+//
+
+    function deactivate ( ) {
+
+        // And there was a place, nothing ever happened at....
+
+    }
+
+    exports.deactivate = deactivate;
+
+// ────────────────────────────────────────────────────────────────────────────────
