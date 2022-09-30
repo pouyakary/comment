bdff84edf6eba4b140447543c2b0d6b88a65193f
Pouya Kary <pouya@kary.us>
2022-09-30 23:58:06 +0330

    compute editor settings

--- src/web/vscode/parameters/editor.ts
+++ src/web/vscode/parameters/editor.ts
@@ -1,29 +1,29 @@
-import * as engine from '../engine';
+import * as engine from '../../engine';
 import * as vscode from 'vscode';
 
 // ─── Compute Environmental Settings ────────────────────────────────────── ✣ ─
 
-export function computeEnvironmentalSettings(): engine.protocols.EnvironmentParameters {
-  const editor =
+export function computeEnvironmentalSettings(): engine.protocols.EditorParameters {
+  const currentEditor =
     vscode.window.activeTextEditor!;
 
   const currentLine =
-    editor.selection.active.line;
+    currentEditor.selection.active.line;
 
   const currentLineString =
-    editor.document.lineAt(currentLine).text;
+    currentEditor.document.lineAt(currentLine).text;
 
   const currentLanguageId =
-    editor.document.languageId;
+    currentEditor.document.languageId;
 
   const currentInsertSpacesStatus =
-    editor.options.insertSpaces === true
+    currentEditor.options.insertSpaces === true
       ? engine.protocols.TabWhitespaceKind.space
       : engine.protocols.TabWhitespaceKind.tab;
 
   const currentTabSize =
-    typeof editor.options.tabSize === 'number'
-      ? editor.options.tabSize
+    typeof currentEditor.options.tabSize === 'number'
+      ? currentEditor.options.tabSize
       : 4;
 
   return {
@@ -34,4 +34,3 @@ export function computeEnvironmentalSettings(): engine.protocols.EnvironmentPara
     languageId:         currentLanguageId,
   };
 }
-
