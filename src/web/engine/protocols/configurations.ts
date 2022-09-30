0e3250d0e84a0bde43554b32f63a010fc1428ffc
Pouya Kary <pouya@kary.us>
2022-09-30 23:57:42 +0330

    user configurations

---
+++ src/web/engine/protocols/configurations.ts
@@ -0,0 +1,8 @@
+// ─── User Settings ─────────────────────────────────────────────────────── ✣ ─
+
+export interface UserSettings {
+  legacyMode:           boolean;
+  ornament:             string;
+  levelOneCommentWidth: number,
+  levelTwoCommentWidth: number,
+}
