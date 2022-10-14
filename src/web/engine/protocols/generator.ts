9d33e99a43ecd35b04b0f7f546a9edc2460a9926
Pouya Kary <pouya@kary.us>
2022-10-15 02:59:19 +0330

    no more needed

--- src/web/engine/protocols/generator.ts
+++
@@ -1,7 +0,0 @@
-import * as protocols from '.';
-
-// ─── Generator Protocol ────────────────────────────────────────────────── ✣ ─
-
-export interface CommentGeneratorProtocol {
-  generate: (context: protocols.GeneratorContext) => string
-}
