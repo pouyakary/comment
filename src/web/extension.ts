0a4230d90a086b578a5c91f6fc417c64ae703761
Pouya Kary ✨ <kary@gnu.org>
2023-03-19 14:53:37 +0330

    min

--- src/web/extension.ts
+++ src/web/extension.ts
@@ -1,15 +1,15 @@
-import * as vscode 				from 'vscode';
+import * as vscode from 'vscode';
 import * as contributions from './vscode/contributions';
 
 // ─── Activation Function ───────────────────────────────────────────────── ✣ ─
 
 export function activate(context: vscode.ExtensionContext) {
-	contributions.registerRenameProviders(context);
-	contributions.registerCommandProviders(context);
+  contributions.registerRenameProviders(context);
+  contributions.registerCommandProviders(context);
 }
 
 // ─── Deactivation Function ─────────────────────────────────────────────── ✣ ─
 
 export function deactivate() {
-	// nothing to do
+  // nothing to do
 }
